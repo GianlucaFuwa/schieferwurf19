d5d974907afc539b976772ffdc3d614826fb11b1
mertduzgun <51233281+mertduzgun@users.noreply.github.com>
2019-06-14 20:06:26 +0200

    Create Widget.h
    Definition der Widget-Klasse

---
+++ Widget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct Widget { // Definition der Widget-Klasse
+	int width, height;  //Parameter zur Bestimmung der Größe und Höhe des Widgets
+	string label;
+	Point xy;
+	Callback do_it;  // führt beim "Drücken" eine Callback-Funktion aus
+	Windows* own;
+	Widget(Point p, int w, int h, string s, Callback cb)
+		: xy(p), width(w), height(h), label(s), do_it(cb){}
+	virtual void attach(Windows&) {}
+};
