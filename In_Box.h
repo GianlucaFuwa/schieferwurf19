b6845d65c161f5b1ef92d5266b583e4c8a3c43f7
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2019-06-16 15:18:21 +0200

    Update In_Box.h

--- In_Box.h
+++ In_Box.h
@@ -1,10 +1,10 @@
 #pragma once
 
 struct In_box : Widget {     //Definition der Widget-Unterklasse In_box.
-	Fl_Input* pw;    // In die Input-Box können Werte wie Winkel etc. eingetragen und als Pointer verwendet werden.
+	Fl_Input* pw;    //Speicheradresse wird benannt.
 	In_box(Point p, int w, int h, string s)    //Parameter zur Bestimmung der Größe und Form der Box werden definiert.
 		: Widget(p, w, h, s, 0) {}
-	void set_value(string s) { pw->value(s.c_str()); }
+	void set_value(string s) { pw->value(s.c_str()); } //Die Input_Box erhält einen string der aus Zahlen besteht, um z.B. den Wurfwinkel oder die Geschwindigkeit festzulegen.
 	string get_value() { return string(pw->value()); }
 	void attach(Windows& window) {
 		pw = new Fl_Input(xy.x, xy.y, width, height, label.c_str());
