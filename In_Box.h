4f0ab9e2c53997d692e630060d54b94d2a2713f2
mertduzgun <51233281+mertduzgun@users.noreply.github.com>
2019-06-16 15:13:36 +0200

    Create In_Box.h
    Definiere die Widget-Unterklasse In_Box, in die Größen zur Konstruktion des schiefen Wurfs eingegeben werden.

---
+++ In_Box.h
@@ -0,0 +1,21 @@
+#pragma once
+
+struct In_box : Widget {     //Definition der Widget-Unterklasse In_box.
+	Fl_Input* pw;    // In die Input-Box können Werte wie Winkel etc. eingetragen und als Pointer verwendet werden.
+	In_box(Point p, int w, int h, string s)    //Parameter zur Bestimmung der Größe und Form der Box werden definiert.
+		: Widget(p, w, h, s, 0) {}
+	void set_value(string s) { pw->value(s.c_str()); }
+	string get_value() { return string(pw->value()); }
+	void attach(Windows& window) {
+		pw = new Fl_Input(xy.x, xy.y, width, height, label.c_str());
+		pw->align(FL_ALIGN_RIGHT); // Eigenschaften wie Farbe der Box etc. werden definiert.
+		pw->color(FL_WHITE);
+		pw->textfont(FL_COURIER);
+		pw->textcolor(FL_BLACK);
+		pw->textsize(12);
+		pw->labelfont(FL_COURIER_ITALIC);
+		pw->labelcolor(FL_WHITE);
+		pw->labelsize(14);
+		own = &window;
+	}
+};
