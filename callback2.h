cf0886f295f5d4fa77393bc1bb221074b5d6a153
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2019-07-02 23:52:23 +0200

    callback2-Funktion für Gnuplot-Plot

---
+++ callback2.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "gnuplot-iostream.h" //gnuplot-iostream um gnuplot fenster zu öffnen.
+
+void callback2(Fl_Widget*, void*) { //callback2-Funktion ruft beim drücken ein gnuplot-Fenster auf, in welchem die aufgenommenen y-Werte gegen die Zeit aufgetragen werden.
+	Gnuplot gp;
+	gp << R"(
+		set terminal wxt size 1000,1000 enhanced font 'Verdana,20'
+		set title 'height vs Time'
+		set xlabel 'Time (s)'
+		set ylabel 'Height'
+		plot 'height_vs_time.txt' w l lw 3 notitle //Werte werden aus der Datei heightvstime.txt aus callback1 genommen
+	)";
+}
